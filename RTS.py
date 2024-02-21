83ad976fadb8fc44756cbbc2833514aae24f877d
Bartek2907 <158321489+Bartek2907@users.noreply.github.com>
2024-02-21 19:11:09 +0100

    Update RTS.py

--- RTS.py
+++ RTS.py
@@ -1,31 +1,26 @@
 import discord
+from discord.ext import commands
 
-# Zmienna intencje przechowuje uprawnienia bota
 intents = discord.Intents.default()
-# Włączanie uprawnienia do czytania wiadomości
 intents.message_content = True
-# Tworzenie bota w zmiennej klienta i przekazanie mu uprawnień
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'Zalogowaliśmy się jako {client.user}')
+    print(f'Zalogowaliśmy się jako {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Cześć!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
-    elif message.content.startswith('$RTS'):
-        await message.channel.send("TYLKO WIDZEW")
-    elif message.content.startswith('$sigma'):
-        await message.channel.send('Sigma male często jest postrzegany jako ktoś, kto żyje według własnych zasad i nie dąży do zdobycia akceptacji lub dominacji w grupie. To osoba, która ceni sobie niezależność, samodzielność i często wybiera własną ścieżkę zamiast podążać za tłumem. Samiec sigma unika konformizmu, co pozwala mu na tworzenie własnej, unikalnej tożsamości.')
-    elif message.content.startswith('$ŁKS'):
-        await message.channel.send("ŁKS głupi pies tak go nie ma a tu jest.")    
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
 
-client.run("token")
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.group()
+async def cool(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+bot.run("token")
