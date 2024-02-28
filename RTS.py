c3d5bc2844ec4fec422da86d65e1a6adb5a0c122
Bartek2907 <158321489+Bartek2907@users.noreply.github.com>
2024-02-28 16:17:41 +0100

    Update RTS.py

--- RTS.py
+++ RTS.py
@@ -13,6 +13,22 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Cześć, jestem bot{bot.user}!')
+async def kushi(ctx):
+    await ctx.send(f'https://www.youtube.com/@PokeTrenerKushi')
+async def jacob(ctx):
+    await ctx.send(f'https://www.youtube.com/@JacobNajlepszy')
+async def kebcio(ctx):
+    await ctx.send(f'https://www.youtube.com/@kebcio')
+async def judi(ctx):
+    await ctx.send(f'https://www.youtube.com/@JudiMakeFun')
+async def monoyek(ctx):
+    await ctx.send(f'https://www.youtube.com/@Manoyek')
+async def kubix(ctx):
+    await ctx.send(f'https://www.youtube.com/@Kubxfn')
+async def smarte(ctx):
+    await ctx.send(f'https://www.youtube.com/@Smarte')
+async def mrbeast(ctx):
+    await ctx.send(f'https://www.youtube.com/@MrBeast')
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
